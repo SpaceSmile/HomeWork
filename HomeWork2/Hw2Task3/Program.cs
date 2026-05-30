9591dc7ef5ffad93a53d65d4992171277ce25dc0
Viktor Sedlachek <v.sedlachek@dodobrands.io>
2026-05-30 10:30:59 +0300

    Add Hw2Task3 and Hw2Task4 projects, update solution files

---
+++ HomeWork2/Hw2Task3/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Hw2Task3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите слово на русском: ");
+        string word = Console.ReadLine();
+
+        switch (word)
+        {
+            case "погода":
+                Console.WriteLine("weather");
+                break;
+            case "дождь":
+                Console.WriteLine("rain");
+                break;
+            case "снег":
+                Console.WriteLine("snow");
+                break;
+            case "солнце":
+                Console.WriteLine("sun");
+                break;
+            case "ветер":
+                Console.WriteLine("wind");
+                break;
+            case "туман":
+                Console.WriteLine("fog");
+                break;
+            case "гроза":
+                Console.WriteLine("thunderstorm");
+                break;
+            case "облако":
+                Console.WriteLine("cloud");
+                break;
+            case "мороз":
+                Console.WriteLine("frost");
+                break;
+            case "радуга":
+                Console.WriteLine("rainbow");
+                break;
+            default:
+                Console.WriteLine($"{word} не найдено в словаре.");
+                break;
+        }
+    }
+}
---
+++ HomeWork2/Hw2Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Hw2Task4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int var1 = int.Parse(Console.ReadLine());
+
+        switch (var1 % 2)
+        {
+            case 0:
+                Console.WriteLine($"{var1} — чётное");
+                break;
+            default:
+                Console.WriteLine($"{var1} — нечётное");
+                break;
+        }
+        
+        Console.Write("Введите число: ");
+        int var2 = int.Parse(Console.ReadLine());
+
+        if (var2 % 2 == 0)
+            Console.WriteLine($"{var2} — чётное");
+        else
+            Console.WriteLine($"{var2} — нечётное");
+    }
+}
