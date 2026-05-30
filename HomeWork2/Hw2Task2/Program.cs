a75af1b62427bc740b5e490494ce7ccc77a870ce
Viktor Sedlachek <v.sedlachek@dodobrands.io>
2026-05-30 10:14:23 +0300

    Add Hw2Task2 project and update solution files

---
+++ HomeWork2/Hw2Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Hw2Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число от 0 до 100: ");
+        int number = int.Parse(Console.ReadLine());
+        
+        switch (number)
+        {
+            case >= 0 and <= 14:
+                Console.WriteLine($"{number} входит в промежуток [0 - 14]");
+                break;
+
+            case >= 15 and <= 35:
+                Console.WriteLine($"{number} входит в промежуток [15 - 35]");
+                break;
+
+            case >= 36 and <= 50:
+                Console.WriteLine($"{number} входит в промежуток [36 - 50]");
+                break;
+
+            case >= 51 and <= 100:
+                Console.WriteLine($"{number} входит в промежуток [51 - 100]");
+                break;
+
+            default:
+                Console.WriteLine($"{number} не входит ни в один из промежутков [0 - 100]");
+                break;
+        }
+    }
+}
