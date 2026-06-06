263e8be789f5f384419da8e5585a66609c3aaf5f
Viktor Sedlachek <v.sedlachek@dodobrands.io>
2026-06-06 07:30:47 +0300

    Task 1

---
+++ HomeWork4/Hw4Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Hw4Task1;
+
+class Program
+{
+    static void Main(string[] args)
+	{
+        int [] massive = { 1, 50, 30 };
+		int? numbers = int.Parse(Console.ReadLine());
+		bool found = false;
+		foreach ( int n in massive)
+		{
+			if (n == numbers)
+            {
+                found = true;
+                break;
+            }
+        }
+		if (found)
+			Console.WriteLine("Число найдено");
+		else
+			Console.WriteLine("Число не найдено");
+	}
+}
