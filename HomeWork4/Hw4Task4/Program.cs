2a1bb7704f50447ad67b6c8364898df0f5cd0836
Viktor Sedlachek <v.sedlachek@dodobrands.io>
2026-06-06 08:23:08 +0300

    Add Hw4Task4 project and implement sum calculation in Program.cs

---
+++ HomeWork4/Hw4Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Hw4Task4;
+
+class Program
+{
+    static void Main(string[] args)
+	{
+		int[,] massive = {
+			{ 1,  1,  1 },
+			{ 1,  1,  1 },
+			{ 1,  1,  1 }
+		};
+		
+		int sum = 0;
+		for (int i = 0; i < massive.GetLength(0); i++)
+		{
+			for (int j = 0; j < massive.GetLength(1); j++)
+			{
+				sum += massive[i, j];
+			}
+		}
+		Console.WriteLine("Сумма всех элементов: " + sum);
+	}
+}
