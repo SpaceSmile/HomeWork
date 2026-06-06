e34e704b7220e5bc46b60e2a7c35c23a0f1b91b1
Viktor Sedlachek <v.sedlachek@dodobrands.io>
2026-06-06 08:32:20 +0300

    Add Hw4Task5 project and implement diagonal output in Program.cs

---
+++ HomeWork4/Hw4Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Hw4Task5;
+
+class Program
+{
+    static void Main(string[] args)
+	{
+		int[,] massive = {
+			{ 1,  2,  3 },
+			{ 4,  5,  6 },
+			{ 7,  8,  9 }
+		};
+        Console.Write("Диагональ: ");
+        for (int i = 0; i < massive.GetLength(0); i++)
+            Console.Write(massive[i, i] + " ");
+	}
+}
